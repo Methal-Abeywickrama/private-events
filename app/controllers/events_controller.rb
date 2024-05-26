ec25bb9d712a7637b862dab9a1a61e91a1c554cd
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-05-26 11:41:20 +0530

    update event controller with create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,25 @@
 class EventsController < ApplicationController
-
+  
   def index 
     @events = Event.all 
+  end
+
+  def new 
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.Event.build(event_params)
+    if @event.save
+      redirect_to @event, notice: "The event was successfully created "
+    else  
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :details, :location, :date)
   end
 end
