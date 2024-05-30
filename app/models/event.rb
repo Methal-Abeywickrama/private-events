19ac6d18ceadce6da7d4b9b8f54a1072d6d45fc1
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-05-30 12:52:40 +0530

    separate past and present events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,15 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances
+
+  # def self.upcoming_events
+  #   where('date >= ?', Date.today).order(date: :asc)
+  # end
+
+  # def self.past_events
+  #   where('date < ?', Date.today).order(date: :desc)
+  # end
+
+  scope :upcoming_events, -> { where('date >= ?', Date.today).order(date: :asc) }
+  scope :past_events, -> { where('date < ?', Date.today).order(date: :desc) }
 end
