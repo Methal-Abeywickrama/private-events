67a580a08218a6cb9b4f021576979013a5369fc2
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-05-30 12:51:53 +0530

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root 'home#index'
+  resources :home
   resources :events do
     resources :event_attendances
   end
